ed37def3eb64061a09642fb1367deaddfa75b8e9
Muhammad Abdullah <abdulah4591@gmail.com>
2021-11-09 00:32:42 +0500

    third commit

---
+++ app/src/main/java/com/example/assigment_2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.assigment_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button1(View view) {
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+
+
+    }
+    public void button2(View view) {
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
